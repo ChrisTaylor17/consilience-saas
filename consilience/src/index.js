44200062bdbf04ef3099b995f6a1daeff39d1ac7
Chris <ChrisTaylor17@gmail.com>
2025-09-19 23:44:08 -0400

    v3.0.0 - Remove all old components, force clean rebuild

--- consilience/src/index.js
+++ consilience/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppNew from './App-new';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppNew />
+    <App />
   </React.StrictMode>
 );
